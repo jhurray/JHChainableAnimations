87b20dfe24da0dcdac134e6f4d763b8f3c8d5235
Bing Liu <biliu@linkedin.com>
2016-12-07 11:17:42 +0800

    rotate method is the same as rotateZ.

--- JHChainableAnimations/UIView+JHChainableAnimations.h
+++ JHChainableAnimations/UIView+JHChainableAnimations.h
@@ -50,7 +50,7 @@
 // These should be used for AutoLayout
 // These should NOT be mixed with properties that affect position and bounds
 - (UIView *) transformIdentity;
-- (JHChainableDegrees) rotate;
+- (JHChainableDegrees) rotate; // Same as rotateZ
 - (JHChainableDegrees) rotateX;
 - (JHChainableDegrees) rotateY;
 - (JHChainableDegrees) rotateZ;
